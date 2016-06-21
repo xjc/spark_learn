48927282537c61b4e29338400c9e97f748296f2d
xjc <xjcbest@live.com>
2016-06-21 15:09:51 +0800

    some functions

--- src/main/scala/com/xjc/sql/rdd2DataFrameTest.scala
+++ src/main/scala/com/xjc/sql/rdd2DataFrameTest.scala
@@ -10,13 +10,15 @@ import org.apache.spark.sql.Row
  */
 
 case class Record(str1: String, str2: String, str3:String, num:Int)
+case class C1(str1:String, str:String, num:String)
 
 object rdd2DataFrameTest {
 
   // Encoders are also created for case classes.
 
   def main(args:Array[String]) {
-    val conf = new SparkConf().setAppName("rdd2DataFrameTest").setMaster("local[2]")
+    //val conf = new SparkConf().setAppName("rdd2DataFrameTest").setMaster("local[2]")
+    val conf = new SparkConf().setAppName("rdd2DataFrameTest")
     val sc = new SparkContext(conf)
     sc.setLogLevel("WARN")
     val ssc = new SQLContext(sc)
@@ -42,7 +44,59 @@ object rdd2DataFrameTest {
     println("!" * 100)
     df3.show
 
+    //plan c
+    val df4 = ssc.createDataFrame(rdd1)
+    df4.printSchema
+    /**
+     * root
+     *  |-- _1: string (nullable = true)
+     *   |-- _2: string (nullable = true)
+     *    |-- _3: string (nullable = true)
+     *     |-- _4: integer (nullable = false)
+     */
+    val df4_1 = ssc.createDataFrame(rdd1).toDF("a", "b", "c", "d")
+    df4_1.printSchema
 
+    val rdd1_2 = rdd1.map(i=>Record(i._1, i._2, i._3, i._4))
+    val df4_2 = ssc.createDataFrame(rdd1_2)
+    df4_2.printSchema
+
+    //plan d  from hive
+    //val df5 = ssc.table("""app.t20160602""")
+    //df5.printSchema
+    /* root
+     *  |-- name: string (nullable = true)
+     *   |-- id: string (nullable = true)
+     */
+
+    val hc = new HiveContext(sc)
+    val df_hive = hc.sql("""select * from app.t20160602""")
+    df_hive.printSchema
+    df_hive.show
+    
+
+    df4_2.registerTempTable("temp")
+    val rdd3 = ssc.sql("select str1, str2, num from temp").
+        map(i=>(i.getString(0), i.getString(1), i.getInt(2))).collect
+    //res84: Array[(String, String, Int)] = Array((a_1,b_1,1), (a_2,b_2,2), (a_3,b_3,3), (a_4,b_4,4), (a_5,b_5,5), (a_6,b_6,6), (a_7,b_7,7), (a_8,b_8,8), (a_9,b_9,9), (a_10,b_10,10))
+    val rdd3_1 = ssc.sql("select str1, str2, num from temp").
+        map(i=>(i.getAs[String](0), i.getAs[String](1), i.getAs[Int](2))).collect
+    //rdd3_1: Array[(String, String, Int)] = Array((a_1,b_1,1), (a_2,b_2,2), (a_3,b_3,3), (a_4,b_4,4), (a_5,b_5,5), (a_6,b_6,6), (a_7,b_7,7), (a_8,b_8,8), (a_9,b_9,9), (a_10,b_10,10))
+    val rdd3_2 = ssc.sql("select str1, str2, num from temp").
+       map(i=>(i.get(0), i.get(1), i.get(2))).collect
+    //rdd3_2: Array[(Any, Any, Any)] = Array((a_1,b_1,1), (a_2,b_2,2), (a_3,b_3,3), (a_4,b_4,4), (a_5,b_5,5), (a_6,b_6,6), (a_7,b_7,7), (a_8,b_8,8), (a_9,b_9,9), (a_10,b_10,10))
+    
+    //fusion
+    import org.apache.spark.sql.Row
+    val rdd3_3 = ssc.sql("select str1, str2, num from temp").
+      map{case Row(a,b,c) => (a,b,c)}
+    //res104: org.apache.spark.rdd.RDD[(Any, Any, Any)] = MapPartitionsRDD[150] at map at <console>:28
+
+
+    val rdd3_3_df = rdd3_3.map(i=>C1(i._1.toString,i._2.toString,i._3.toString)).toDF
+    rdd3_3_df.registerTempTable("temp1")
+    ssc.sql("select * from temp1").show
+    
     sc.stop
   }
 }
